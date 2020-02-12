af321425d9df3fdde56f196284ad501138e379ea
stealth90 <41144486+stealth90@users.noreply.github.com>
2020-02-12 09:24:58 +0100

    fixed interceptor

--- src/app/auth/token-interceptor.service.ts
+++ src/app/auth/token-interceptor.service.ts
@@ -16,7 +16,7 @@ export class TokenInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any> , next: HttpHandler): Observable<HttpEvent<any>> {
     req = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.authService.getToken()}`
+        Authorization: this.authService.getToken()
       }
     });
     return next.handle(req)
