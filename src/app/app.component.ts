0ce39af0a7d5e662111674f4870a058d27e5fdbc
domenicosf92 <signorefioredomenico@tiscali.it>
2020-02-18 18:19:02 +0100

    Fixed toast. Now it works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform, NavController } from '@ionic/angular';
+import { Platform, NavController, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './auth/auth.service';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
     private authService: AuthService,
-    private navController: NavController) {
+    private navController: NavController,
+    private socket: Socket,
+    private toastCtrl: ToastController) {
     this.initializeApp();
   }
+
+  async showToast(event) {
+    const toast = await this.toastCtrl.create({
+      message: event,
+      position: 'bottom',
+      duration: 4000
+    });
+    toast.present();
+  }
+
+  ngOnInit() {
+    this.socket.connect();
+    this.socket.fromEvent('new-status').subscribe( (data: object & { event: string, status: any}) => {
+      if (!this.authService.isRestaurant()) {
+        this.showToast(`Status ${data.event} to ${data.status.status}`);
+      }
+    });
+  }
+
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
--- src/app/restaurants/restaurants.page.ts
+++ src/app/restaurants/restaurants.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-import { Socket } from 'ngx-socket-io';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restaurants',
@@ -11,26 +9,9 @@ import { ToastController } from '@ionic/angular';
 export class RestaurantsPage implements OnInit {
 
   constructor(
-    private authService: AuthService,
-    private socket: Socket,
-    private toastCtrl: ToastController) { }
-
-  async showToast(event) {
-    const toast = await this.toastCtrl.create({
-      message: event,
-      position: 'bottom',
-      duration: 4000
-    });
-    toast.present();
-  }
+) { }
 
   ngOnInit() {
-    this.socket.connect();
-    this.socket.fromEvent('new-status').subscribe( (data: object & { event: string, status: any}) => {
-      if (!this.authService.isRestaurant()) {
-        this.showToast(`Status ${data.event} to ${data.status.status}`);
-      }
-    });
   }
 
 }
